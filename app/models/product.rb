880e2bb85d1cf7d410631a67d19442361770b9be
Tucq <tucq88@gmail.com>
2016-10-25 15:07:38 +0000

    Half of Milestone 5

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,17 @@
 class Product < ActiveRecord::Base
-	def self.alphabetical
-    order(:name)
-  end
+    def self.alphabetical
+        order(:name)
+    end
+
+    def discount_amount
+        price = self.price_vnd
+        if price > 100000
+          (price * 0.79)
+        # elseif price > 200000
+        #   price * 0.69
+        # elseif price > 800000
+        #   price * 0.59
+        end
+
+    end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,16 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-	describe "#alphabetical" do
-		before do 
-      @product1 = Product.create!(:name => "aaaa")
-      @product2 = Product.create!(:name => "cccc")
-      @product3 = Product.create!(:name => "bbbb")
+    describe "#alphabetical" do
+        before do
+            @product1 = Product.create!(:name => "aaaa", :price_vnd => 150000)
+            @product2 = Product.create!(:name => "cccc", :price_vnd => 250000)
+            @product3 = Product.create!(:name => "bbbb", :price_vnd => 900000)
+        end
+
+        it "let user sort products" do
+            expect(Product.alphabetical).to eq [@product1, @product3, @product2]
+        end
     end
 
-    it "let user sort products" do
-      expect(Product.alphabetical).to eq [@product1, @product3, @product2]
+    describe "#discount_amount" do
+
+        before do
+            @product1 = Product.create!(:name => "aaaa", :price_vnd => 150000)
+            @product2 = Product.create!(:name => "cccc", :price_vnd => 250000)
+            @product3 = Product.create!(:name => "bbbb", :price_vnd => 900000)
+        end
+
+        it "let product discount" do
+            expect(@product1.discount_amount).to eq 120000
+            # expect(@product2.discount_amount).to eq 120000
+            # expect(@product3.discount_amount).to eq 120000
+        end
     end
-  end
+
 
 end
